d57cfd61eb503c6222ec9b4419bccb7e8b47224e
Anabil <91933794+Anabil-Baruah@users.noreply.github.com>
2023-04-06 22:16:15 +0530

    Strted to make admin page

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,19 @@
 const router = require('express').Router()
-const auth = require('../auth')
+
 const service = require('../models/services')
 const blog = require('../models/Blogs')
 const work = require('../models/work')
 
 router.route('/')
-    .get( async(req,res)=>{
+    .get(async (req, res) => {
         var services = await service.find({})
         var works = await work.find({})
         var blogs = await blog.find({})
         works = JSON.parse(JSON.stringify(works))
         blogs = JSON.parse(JSON.stringify(blogs))
         services = JSON.parse(JSON.stringify(services))
-
-        res.render('admin', {services, works, blogs})
+        res.render('admin', { services, blogs, works })
     })
+
 
 module.exports = router
